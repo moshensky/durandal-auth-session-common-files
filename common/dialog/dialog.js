e8e8b8ab81ebb8eafd8d31a4b6470b92b905010c
Nikita Moshensky <nikita@moshensky.com>
2015-09-05 19:43:50 +0300

    add fourth button to dialog

--- common/dialog/dialog.js
+++ common/dialog/dialog.js
@@ -1,72 +1,81 @@
 define(['plugins/dialog', 'knockout', 'i18n', 'jquery'],
-    function (Dialog, ko, i18n, $) {
-        'use strict';
+  function (Dialog, ko, i18n, $) {
+    'use strict';
 
-        var CustomDialog = function (opts) {
-            this.title = opts.title;
-            this.msg = opts.msg;
-            this.model = opts.model || {};
+    var CustomDialog = function (opts) {
+      this.title = opts.title;
+      this.msg = opts.msg;
+      this.model = opts.model || {};
 
-            this.okBtnText = opts.okBtnText || i18n.t('app:common.ok');
-            this.okBtnClasses = ko.observable(opts.okBtnClasses || 'btn btn-default');
+      this.okBtnText = opts.okBtnText || i18n.t('app:common.ok');
+      this.okBtnClasses = ko.observable(opts.okBtnClasses || 'btn btn-default');
 
-            this.showCancelBtn = opts.showCancelBtn || false;
-            this.cancelBtnText = opts.cancelBtnText || i18n.t('app:common.cancel');
-            this.cancelBtnClasses = opts.cancelBtnClasses || 'btn btn-default';
+      this.showCancelBtn = opts.showCancelBtn || false;
+      this.cancelBtnText = opts.cancelBtnText || i18n.t('app:common.cancel');
+      this.cancelBtnClasses = opts.cancelBtnClasses || 'btn btn-default';
 
-            this.showThirdBtn = opts.showThirdBtn || false;
-            this.thirdBtnText = opts.thirdBtnText || 'Third Button';
-            this.thirdBtnClasses = opts.thirdBtnClasses || 'btn btn-default';
-            this.thirdBtnReturnStatus = opts.thirdBtnReturnStatus || undefined;
+      this.showThirdBtn = opts.showThirdBtn || false;
+      this.thirdBtnText = opts.thirdBtnText || 'Third Button';
+      this.thirdBtnClasses = opts.thirdBtnClasses || 'btn btn-default';
+      this.thirdBtnReturnStatus = opts.thirdBtnReturnStatus || undefined;
 
-            this.customWidth = opts.customWidth;               
-        };
+      this.showFourthBtn = opts.showFourthBtn || false;
+      this.fourthBtnText = opts.fourthBtnText || 'Fourth Button';
+      this.fourthBtnClasses = opts.fourthBtnClasses || 'btn btn-default';
+      this.fourthBtnReturnStatus = opts.fourthBtnReturnStatus || 'fourthButtonClicked';
 
-        CustomDialog.prototype = {
-            show: function () {
-                return Dialog.show(this);
-            },
-            ok: function () {
-                var status = true;
-                this._executeAction(status);
-            },
-            cancel: function () {
-                var status = false;
-                this._executeAction(status);
-            },
-            third: function () {
-                var status = this.thirdBtnReturnStatus;
-                this._executeAction(status);
-            },
-            _executeAction: function (status) {
-                var executeAction = true;
-                var executeActionPromise;
+      this.customWidth = opts.customWidth;
+    };
 
-                if (typeof this.model.onDialogButtonClick === 'function') {
-                    executeAction = this.model.onDialogButtonClick(status);
+    CustomDialog.prototype = {
+      show: function () {
+        return Dialog.show(this);
+      },
+      ok: function () {
+        var status = true;
+        this._executeAction(status);
+      },
+      cancel: function () {
+        var status = false;
+        this._executeAction(status);
+      },
+      third: function () {
+        var status = this.thirdBtnReturnStatus;
+        this._executeAction(status);
+      },
+      fourth: function () {
+        var status = this.fourthBtnReturnStatus;
+        this._executeAction(status);
+      },
+      _executeAction: function (status) {
+        var executeAction = true;
+        var executeActionPromise;
 
-                    if (executeAction) {
-                        Dialog.close(this, this.model, status);
-                    }
-                } else if (typeof this.model.onDialogButtonClickSync === 'function') {
-                    var self = this;
+        if (typeof this.model.onDialogButtonClick === 'function') {
+          executeAction = this.model.onDialogButtonClick(status);
 
-                    executeActionPromise = this.model.onDialogButtonClickSync(status);
+          if (executeAction) {
+            Dialog.close(this, this.model, status);
+          }
+        } else if (typeof this.model.onDialogButtonClickSync === 'function') {
+          var self = this;
 
-                    if (executeActionPromise !== undefined) {
-                        executeActionPromise.then(function (allowDialogClose) {
-                            if (allowDialogClose === true) {
-                                Dialog.close(self, self.model, status);
-                            }
-                        });
-                    } else {
-                        Dialog.close(this, this.model, status);
-                    }
-                } else {
-                    Dialog.close(this, this.model, status);
-                }
-            }
-        };
+          executeActionPromise = this.model.onDialogButtonClickSync(status);
 
-        return CustomDialog;
-    });
+          if (executeActionPromise !== undefined) {
+            executeActionPromise.then(function (allowDialogClose) {
+              if (allowDialogClose === true) {
+                Dialog.close(self, self.model, status);
+              }
+            });
+          } else {
+            Dialog.close(this, this.model, status);
+          }
+        } else {
+          Dialog.close(this, this.model, status);
+        }
+      }
+    };
+
+    return CustomDialog;
+  });
